e1464b83e631a0d041de76d8028924c6e49273f4
r-tamura <rtamura30@gmail.com>
2021-10-04 23:12:33 +0900

    day08: カーネルメモリ管理に必要なセットアップ

---
+++ kernel/paging.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file paging.hpp
+ *
+ * メモリページング用のプログラムを集めたファイル
+ */
+
+#pragma once
+
+#include <cstddef>
+
+/** @brief 静的に確保するページディレクトリの個数
+ *
+ * 個の定数は SetupIdentitypageMap で使用される.
+ * 1つのページディレクトリには512個の2MiBページを設定できるので,
+ * kPageDirectoryCount x 1GiBの仮想アドレスがマッピングされることになる.
+ */
+const size_t kPageDirectoryCount = 64;
+/** @brief 仮想アドレス=物理アドレスとなるようにページテーブルを設定する.
+ * 最終的に CR3 レジスタが正しく設定されたページテーブルを指すようになる.
+ */
+void SetupIdentityPageTable();
