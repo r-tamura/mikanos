fd3c1ff5640f499dae79e49d7733948a4b58ea16
r-tamura <rtamura30@gmail.com>
2021-10-19 20:23:52 +0900

    day15: 競合によるウィンドウの表示乱れの修正  - メインタスクだけに描画処理をさせるように

--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -12,6 +12,7 @@
 
 #include "graphics.hpp"
 #include "window.hpp"
+#include "message.hpp"
 
 /**
  * @brief Layerは1つの層を表す。
@@ -72,3 +73,4 @@ class LayerManager {
 extern LayerManager* layer_manager;
 
 void InitializeLayer();
+void ProcessLayerMessage(const Message& msg);
