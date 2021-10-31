a7f209cb8ee3476bfc2e395ec81cf7b9ac798fc3
r-tamura <rtamura30@gmail.com>
2021-10-31 15:53:43 +0900

    day21: 終了用システムコールでアプリを終了できるように - システムコールを呼ばないアプリの対応が課題

--- apps/rpn/rpn.cpp
+++ apps/rpn/rpn.cpp
@@ -16,7 +16,9 @@ void Push(long value) {
   stack[stack_ptr] = value;
 }
 
-extern "C" int main(int argc, char** argv) {
+extern "C" void SyscallExit(int exit_code);
+
+extern "C" void main(int argc, char** argv) {
   stack_ptr = -1;
 
   for (int i = 1; i < argc; ++i) {
@@ -34,15 +36,11 @@ extern "C" int main(int argc, char** argv) {
     }
   }
 
-  if (stack_ptr < 0) {
-    return 0;
-  }
   long result = 0;
   if (stack_ptr >= 0) {
     result = Pop();
   }
 
   printf("%ld\n", result);
-  while (1);
-  // return static_cast<int>(Pop());
+  SyscallExit(static_cast<int>(result));
 }
