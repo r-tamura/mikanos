f096452db3981e0ad043f5d46dcb116742c0862a
r-tamura <rtamura30@gmail.com>
2021-11-06 14:27:39 +0900

    day24: アクティブレイヤーのターミナルのみカーソルが点滅するように

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -10,6 +10,7 @@
 #include "elf.hpp"
 #include "memory_manager.hpp"
 #include "paging.hpp"
+#include "timer.hpp"
 
 namespace {
 
@@ -551,10 +552,18 @@ void TaskTerminal(uint64_t task_id, int64_t data) {
   Task& task = task_manager->CurrentTask();
   Terminal* terminal = new Terminal{task_id};
   layer_manager->Move(terminal->LayerID(), {100, 200});
-  active_layer->Activate(terminal->LayerID());
   layer_task_map->insert(std::make_pair(terminal->LayerID(), task_id));
+  active_layer->Activate(terminal->LayerID());
   (*terminals)[task_id] = terminal;
   __asm__("sti");
+
+  auto add_blink_timer = [task_id](unsigned long t){
+    timer_manager->AddTimer(Timer{t + static_cast<int>(kTimerFreq * 0.5),
+                                  1, task_id});
+  };
+  add_blink_timer(timer_manager->CurrentTick());
+
+  bool window_isactive = false;
 
   while (true) {
     __asm__("cli");
@@ -568,7 +577,8 @@ void TaskTerminal(uint64_t task_id, int64_t data) {
 
     switch (msg->type) {
     case Message::kTimerTimeout:
-      {
+      add_blink_timer(msg->arg.timer.timeout);
+      if (window_isactive) {
         const auto area = terminal->BlinkCursor();
         Message msg = MakeLayerMessage(
             task_id, terminal->LayerID(), LayerOperation::DrawArea, area);
@@ -588,6 +598,9 @@ void TaskTerminal(uint64_t task_id, int64_t data) {
         task_manager->SendMessage(1, msg);
         __asm__("sti");
       }
+      break;
+    case Message::kWindowActive:
+      window_isactive = msg->arg.window_activate.activate;
       break;
     default:
       break;
