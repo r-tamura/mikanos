c7e246b60f2a4816ff2c8b4dc2d7322e64910538
r-tamura <rtamura30@gmail.com>
2021-11-09 22:55:52 +0900

    day26: アプリが新規ファイルを作成できるように

--- kernel/error.hpp
+++ kernel/error.hpp
@@ -32,6 +32,8 @@ class Error {
       kInvalidFormat,
       kFrameTooSmall,
       kInvalidFile,
+      kIsDirectory,
+      kNoSuchEntry,
       kLastOfCode,  // この列挙子は常に最後に配置する
     };
 
@@ -61,8 +63,10 @@ class Error {
       "kNoPCIMSI",
       "kNoSuchTask",
       "kInvalidFormat",
-      "kInvalidFile",
       "kFrameTooSmall",
+      "kInvalidFile",
+      "kIsDirectory",
+      "kNoSuchEntry",
     };
     static_assert(Error::Code::kLastOfCode == code_names_.size());
 
